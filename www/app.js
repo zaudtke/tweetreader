452f9358e1b00d200493c7c46c37849171a88052
Al Zaudtke <zaudtke@gmail.com>
2018-07-01 12:11:28 -0400

    add initial tweet

--- www/app.js
+++ www/app.js
@@ -1,7 +1,16 @@
+let initial = {
+    profileImageUrl : "https://pbs.twimg.com/profile_images/913591850576969730/lQx8iaMm_normal.jpg",
+    name : "THAT Conference",
+    screenName : "ThatConference",
+    createdAt : "Tue Jun 19 11:48:00 +0000 2018",
+    text : "We’re geeking out over this year’s #THATConference keynotes. @jessitron @housecor @reverentgeek and @jlengstorf are rocking the big stage this year. So much awesome will be shared.",
+    imageUrl : "https://pbs.twimg.com/media/DgELVFPUcAAO-ya.jpg"
+}
+
 
 let viewModel = {
     refreshUrl : '',
-    tweets : []
+    tweets : [initial]
 }
 
 const animations = ["bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp", "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig", "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "flip", "flipInX", "flipInY", "lightSpeedIn", "rotateIn", "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "slideInUp", "slideInDown", "slideInLeft", "slideInRight", "zoomIn", "zoomInDown", "zoomInLeft", "zoomInRight", "zoomInUp", "rollIn"];
@@ -38,6 +47,7 @@ function getTags(){
 }
 
 function bootstrapApp(){
+    selectTweet();
     getTags();
     authenticate();
     window.setInterval(selectTweet,10000);
